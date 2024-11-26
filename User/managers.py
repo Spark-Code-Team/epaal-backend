7dc74f59e8c47ff05e6aa6eac957e2e8be83631d
rezaebadi <rezaaebadi1381@gmail.com>
2024-11-26 17:10:04 +0330

    change user manager function

--- User/managers.py
+++ User/managers.py
@@ -9,7 +9,7 @@ def int_to_unique_string(num):
     return hex_str
 
 class UserManager(BaseUserManager):
-    def create_user(self,phone_number,first_name=None,last_name=None, national_code=None,password=None,gender=None,inviter_id=None,role=None):
+    def create_user(self,phone_number,first_name=None,last_name=None, national_code=None,password=None,is_man=None,inviter=None,role=None):
 
         if not phone_number:
             raise ValueError("Users must have an phone number")
@@ -20,8 +20,8 @@ class UserManager(BaseUserManager):
             phone_number=phone_number,
             first_name=first_name,
             last_name=last_name,
-            gender=gender,
-            inviter_id=inviter_id,
+            is_man=is_man,
+            inviter=inviter,
             role=role,
         )
         if password is not None:
@@ -32,18 +32,20 @@ class UserManager(BaseUserManager):
         return user
 
     
-    def create_superuser(self, national_code, password,phone_number,gender,first_name,last_name, role):
+    def create_superuser(self,phone_number, national_code=None, password=None,role=Role.objects.get(name="admin")):
         user = self.create_user(
             first_name="admin",
             last_name="admin",
-            gender=gender,
+            is_man=True,
             phone_number=phone_number,
             national_code=national_code,
             password=password,
-            inviter_id=None,
-            role=Role.objects.get(name="admin"),
+            role=Role.objects.get(name="admin")
         )
         user.is_admin = True
         user.is_superuser = True
+        user.has_two_factor = True
+
+
         user.save(using=self._db)
         return user
