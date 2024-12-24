f6088f5412adaa9f5f5480a1923e78c81ae4a02d
rezaebadi <rezaaebadi1381@gmail.com>
2024-12-24 15:03:08 +0330

    ALLCategoryView done

--- Product/urls.py
+++ Product/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include, re_path
 
-from .views import ALLCategory
+from .views import ALLCategoryView,ShopLandingView
 app_name = 'Product'
 urlpatterns = [
 
-    path("all_categories", ALLCategory.as_view(), name="all_categories"),
+    path("all_categories", ALLCategoryView.as_view(), name="all_categories"),
+    path("shop_landing", ShopLandingView.as_view(), name="shop_landing"),
 
 ]
--- Product/views.py
+++ Product/views.py
@@ -7,10 +7,15 @@ from .serializers import All_ToplevelSerializer
 from .models import ToplevelTopic
 # Create your views here.
 
-class ALLCategory(APIView):
+class ALLCategoryView(APIView):
     def get(self, request):
         if ToplevelTopic.objects.filter().exists():
             ser_data=All_ToplevelSerializer(instance=ToplevelTopic.objects.filter(),many=True)
             return Response({"toplevel_topic":ser_data.data},status=status.HTTP_200_OK)
         else:
             return Response({"error":"there is not any top level topic"},status=status.HTTP_204_NO_CONTENT)
+        
+class ShopLandingView(APIView):
+    def get(self, request):
+        pass
+        
