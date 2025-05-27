12015a39bd8fba6a9ade69ed7748111841df6a0a
rezaebadi <rezaaebadi1381@gmail.com>
2025-05-27 13:46:33 +0330

    test cicd

--- Product/views.py
+++ Product/views.py
@@ -233,6 +233,7 @@ class GetMidlevelTopic(APIView):
             filter_kwargs["toplevel_topic"] = request.data["toplevel_topic"]
         if(request.data.get("is_product") is not None) and( request.data["is_product"] is not ""):
             filter_kwargs["is_product"] = request.data["is_product"]
+        print("hiiiiiiiiiiiiiiiiiiiiiiiiii")
         if MidlevelTopic.objects.filter(**filter_kwargs).exists():
             ser_data=MidlevelTopicSerializer(MidlevelTopic.objects.filter(**filter_kwargs),context={"request":request},many=True)
             return Response({"data":ser_data.data},status=status.HTTP_200_OK)
