471c03aef636d055fa32805f660f91c2a3db6905
behzad 1k <bhzd1k@gmail.com>
2026-01-28 11:39:07 +0330

    evaam

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
+
+# Activate virtual environment
+source /opt/epaal-backend/.venv/bin/activate
+
+# Navigate to project directory
+cd /opt/epaal-backend
+
+# Run Django commands
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+
+# Start Gunicorn
 exec gunicorn EvaamBack.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 120
